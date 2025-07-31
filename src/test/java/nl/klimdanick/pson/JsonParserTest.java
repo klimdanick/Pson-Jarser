a37e382f1dccb497a6cc24bf36d3dc1c59f41bf9
Sascha <1032062@hr.nl>
2025-07-31 21:23:26 +0200

    cleanup

--- src/test/java/nl/klimdanick/pson/JsonParserTest.java
+++ src/test/java/nl/klimdanick/pson/JsonParserTest.java
@@ -123,59 +123,7 @@ public class JsonParserTest {
 	public void testNestedMix() {
 		String jsonString = """
 		{
-		  "nested_mix": {
-			"a": [
-			  1,
-			  {
-				"b": [
-				  2,
-				  {
-					"c": [
-					  3,
-					  {
-						"d": [
-						  4,
-						  {
-							"e": [
-							  5,
-							  {
-								"f": [
-								  6,
-								  {
-									"g": [
-									  7,
-									  {
-										"h": [
-										  8,
-										  {
-											"i": [
-											  9,
-											  {
-												"j": [
-												  10,
-												  {
-													"k": "deep"
-												  }
-												]
-											  }
-											]
-										  }
-										]
-									  }
-									]
-								  }
-								]
-							  }
-							]
-						  }
-						]
-					  }
-					]
-				  }
-				]
-			  }
-			]
-		  }
+			"nested_mix":{"a":[1,{"b":[2,{"c":[3,{"d":[4,{"e":[5,{"f":[6,{"g":[7,{"h":[8,{"i":[9,{"j":[10,{"k":"deep"}]}]}]}]}]}]}]}]}]}]}
 		}
 		""";
 
@@ -263,7 +211,7 @@ public class JsonParserTest {
 		IllegalStateException thrown = assertThrows(
 				IllegalStateException.class,
 				() -> Pson.fromString(jsonString), // code that should throw
-				"Expected doSomething(null) to throw, but it didn't"
+				"Expected fromString(String) to throw, but it didn't"
 		);
 	}
 }
